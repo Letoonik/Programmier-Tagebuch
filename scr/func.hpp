76917ede726adefb0dce904f3968aa4b7e96289d
Letoonik <leonsandolski2007@gmail.com>
2022-08-27 14:40:20 +0200

    v1.1.1
    Neue Dateiorganisation + Icon

---
+++ scr/func.hpp
@@ -0,0 +1,46 @@
+/* Letoonik © 2022
+    Simpler Programmier-Tagestracker
+    "Hast du heute schon programmiert?" */
+
+#include <iostream>
+#include <fstream>
+#include <ctime>
+#include <vector>
+#include <conio.h>
+
+using namespace std;
+
+int menu(){
+    int mode;
+    cout << "Programmier-Tagebuch\n";
+    cout << "----------------------------------\n";
+    cout << "1. Neuer Eintrag verfassen\n";
+    cout << "2. Letzer Eintrag loeschen\n";
+    cout << "3. Programm schliessen\n";
+    cout << "----------------------------------\n";
+    cout << "Auswahl: ";
+    cin >> mode;
+    return mode;
+}
+
+void deleteLastDiaryEntry() {
+
+string line; 
+vector<string> lines;
+std::ifstream inputStream("hdhsp.txt");
+
+while (getline(inputStream,line)) {
+    lines.push_back(line);
+}
+inputStream.close();
+
+std::fstream outputStream("hdhsp.txt", ios::out | ios::trunc);
+if (outputStream.is_open())
+{
+    for (int i=0; i < lines.size()-2; i++)
+    {
+        outputStream << lines[i] << "\n";
+    }
+    outputStream.close();
+}
+}
---
+++ scr/hdhsp.cpp
@@ -0,0 +1,67 @@
+/* Letoonik © 2022
+    Simpler Programmier-Tagestracker
+    "Hast du heute schon programmiert?" */
+
+#include <iostream>
+#include <fstream>
+#include <ctime>
+#include <vector>
+#include "func.hpp"
+
+using namespace std;
+
+int main(){
+    string answer;
+    string progLang;
+    int valMenu;
+
+    std::ofstream outfile;
+    outfile.open("hdhsp.txt", std::ios_base::app); //öffnet "hdhsp.txt",falls nicht vorhanden, wird die Datei erstellt
+    
+    std::time_t t = std::time(0); // Fragt jetzige Unix-Zeit ab
+    std::tm* now = std::localtime(&t); //Konvertiert Zeit
+
+
+valMenu = menu();
+system("cls");
+if (valMenu == 1) {
+    
+    cout << "Hast du heute schon programmiert?(y/n):";
+    cin >> answer;
+
+if(answer == "y") {
+        cout << "In welcher Programiersprache hast du programmiert?: ";
+        cin >> progLang;
+        outfile << now->tm_mday  << '.' << now->tm_mon + 1 << '.' << now->tm_year + 1900 << ": Programmiert mit " << progLang <<".\n\n"; //Speichert Zeit und Programiertag + Programmiersprache ab 
+        cout << "Gut gemacht ^_^\n";
+        system("pause");
+        system("cls");
+        system("hdhsp.exe");
+    }
+
+    else if (answer == "n") {
+        cout << "Nicht so gut, mach weiter >:( \n";
+        outfile << now->tm_mday  << '.' << now->tm_mon + 1 << '.' << now->tm_year + 1900 << ": NICHT programmiert.\n\n"; //dat gleiche wie oben, nur das man eben nicht programmiert hat
+        system("pause");
+        system("cls");
+        system("hdhsp.exe");
+    } 
+    }
+
+    else if(valMenu == 2) {
+        string sure;
+        cout << "Bist du dir sicher, dass du den letzten Eintrag loeschen willst?(y/n):";
+        cin >> sure;
+        if(sure == "y") {
+        deleteLastDiaryEntry();
+        system("cls");
+        cout << "Letzter Eintrag wurde geloescht!\n";
+        system("pause");
+        system("cls");
+        system("hdhsp.exe");
+        }
+    }
+    else if(valMenu == 3) {
+        return 0;
+    }
+}
